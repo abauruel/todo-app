c2b3cc300206bfc8ea541c479c122b9d72a7472b
Alex.Claude <alex.claude@cmng.com.br>
2018-08-21 10:00:51 -0300

    alteração de teste

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,7 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
-export default props => (
+export default props =>  (
   <div className='container'>
   <h1>Teste</h1>
   </div>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
             loader: ExtractTextPlugin.extract('style-loader','css-loader')
           }, {
               test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-              loader: 'file'
+              loader:  'file'
           }
         ]
     }
